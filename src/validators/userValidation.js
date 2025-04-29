db305bd4ebaddac7f0b1af868def7190332fe12d
blvckjoy <sirjeggz@gmail.com>
2025-04-29 01:48:52 +0100

    feat: add role validation in user schema

--- src/validators/userValidation.js
+++ src/validators/userValidation.js
@@ -5,6 +5,7 @@ function validateUser(user) {
       username: Joi.string().min(5).max(15).required(),
       email: Joi.string().email().required(),
       password: Joi.string().min(8).max(15).required(),
+      role: Joi.string().valid("admin", "basic").required(),
    });
    return schema.validate(user);
 }
